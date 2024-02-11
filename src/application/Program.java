acf6d6d2aef6990f5e79d7ea4e456289a2c2330b
railton <railtonplayrx@gmail.com>
2024-02-11 09:47:08 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
         Department obj = new Department("Books",1);
         Seller seller = new Seller(21,"Railton","Railton2@gmail.com",new Date(),3500.0);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        
         System.out.println(seller);
     }
 }
