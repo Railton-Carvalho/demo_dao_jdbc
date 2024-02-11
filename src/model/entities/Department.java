59a8d3bae2d4c1e46dcd97839d00872bac58178c
railton <railtonplayrx@gmail.com>
2024-02-11 08:52:19 -0300

    Updating some application class

---
+++ src/model/entities/Department.java
@@ -0,0 +1,54 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+    private String name;
+    private Integer id;
+
+    public Department(){
+
+    }
+    public Department(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
